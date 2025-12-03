4dd8d48bfe8b2b059ed22c9867bfc6941db1fbe9
azpun <azpunari@gmail.com>
2025-12-03 17:30:05 +0700

    feat: update display structure and add button event listeners for calculator functionality
    refactor: delete element button and plan to rebuilt button using div, with reason: have bug when using element html button when change value on display caculator

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import './style.css'
 
+function getEleId(id) {
+    document.getElementById(id);
+}
+const displayText = document.getElementById('display');
+
+const numOne = document.getElementById('btn-One');
+const numTwo = document.getElementById('btn-two');
+const numThree = document.getElementById('btn-three');
+const numFour = document.getElementById('btn-four');
+const numFive = document.getElementById('btn-five');
+const numSix = document.getElementById('btn-six');
+const numSeven = document.getElementById('btn-seven');
+const numEight = document.getElementById('btn-eight');
+const numNine = document.getElementById('btn-nines')
+
+numOne.addEventListener('click', () => {
+    document.getElementById('display').innerText = 1
+})
