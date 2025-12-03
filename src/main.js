d9c357db86a5a13037347317f2488df5af989a0c
azpun <azpunari@gmail.com>
2025-12-03 22:44:02 +0700

    feat: restructure button layout and enhance event handling for calculator buttons

--- src/main.js
+++ src/main.js
@@ -1,20 +1,40 @@
 import './style.css'
 
-function getEleId(id) {
-    document.getElementById(id);
-}
-const displayText = document.getElementById('display');
 
-const numOne = document.getElementById('btn-One');
-const numTwo = document.getElementById('btn-two');
-const numThree = document.getElementById('btn-three');
-const numFour = document.getElementById('btn-four');
-const numFive = document.getElementById('btn-five');
-const numSix = document.getElementById('btn-six');
-const numSeven = document.getElementById('btn-seven');
-const numEight = document.getElementById('btn-eight');
-const numNine = document.getElementById('btn-nines')
+const displayText = document.querySelector('#display');
+const numButton = document.querySelectorAll('.btn-num');
 
-numOne.addEventListener('click', () => {
-    document.getElementById('display').innerText = 1
+numButton.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const value = btn.dataset.num;
+        if (displayText.textContent === '0') {
+            displayText.textContent = value;    
+        } else {
+            displayText.textContent += value;
+        };
+    })
 })
+
+// numOne.addEventListener('click', () => {
+//     if (displayText.textContent === '0') {
+//         displayText.textContent = 1;    
+//     } else {
+//         displayText.textContent += 1;
+//     };
+// });
+
+// numTwo.addEventListener('click',() => {
+//     if (displayText.textContent === '0') {
+//         displayText.textContent = 2;    
+//     } else {
+//         displayText.textContent += 2;
+//     };
+// });
+
+// numThree.addEventListener('click',() => {
+//     if (displayText.textContent === '0') {
+//         displayText.textContent = 3;    
+//     } else {
+//         displayText.textContent += 3;
+//     };
+// });
