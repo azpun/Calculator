bde9c437be32b40276f1ea816ba24038dcd130c5
azpun <azpunari@gmail.com>
2025-12-04 17:28:49 +0700

    feat: add backspace button functionality and style for improved user interaction

--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
 import './style.css'
 
 
-const displayText = document.querySelector('#display');
-const numButton = document.querySelectorAll('.btn-num');
+const displayText = document.querySelector('#display'); // querySelector to select element with id display
+const numButton = document.querySelectorAll('.btn-num'); // querySelectorAll to select all elements with class btn-num
 const btnClear = document.getElementById('btn-clear');
+const btnBackspace = document.getElementById('btn-backspace');
 
 // event and function button clear
 btnClear.addEventListener('click', () => {
     displayText.textContent = '0';
 });
 
+// event and function to delete/backspace
+btnBackspace.addEventListener('click', () => {
+    if (displayText.textContent.length === 1) { // if only one character left, reset to 0
+        displayText.textContent = '0';
+    } else {
+        displayText.textContent = displayText.textContent.slice(0, -1); // slice method to remove last character, slice(0, -1) means start from index 0 to the second last character.
+    } 
+    // why displayText.textContent can use slice method? because displayText.textContent is a string, and string has slice method.
+});
+
+// event and function for number buttons 0-9
+// using forEach because numButton used querySelectorAll which returns a node list
+// node list is similar to an array, so we can use forEach to loop through each button
 numButton.forEach(btn => {
     btn.addEventListener('click', () => {
-        const value = btn.dataset.num;
+        const value = btn.dataset.num; // dataset.num to get the value of data-num attribute from the button
         if (displayText.textContent === '0') {
             displayText.textContent = value;    
         } else {
@@ -21,26 +35,3 @@ numButton.forEach(btn => {
     })
 })
 
-// numOne.addEventListener('click', () => {
-//     if (displayText.textContent === '0') {
-//         displayText.textContent = 1;    
-//     } else {
-//         displayText.textContent += 1;
-//     };
-// });
-
-// numTwo.addEventListener('click',() => {
-//     if (displayText.textContent === '0') {
-//         displayText.textContent = 2;    
-//     } else {
-//         displayText.textContent += 2;
-//     };
-// });
-
-// numThree.addEventListener('click',() => {
-//     if (displayText.textContent === '0') {
-//         displayText.textContent = 3;    
-//     } else {
-//         displayText.textContent += 3;
-//     };
-// });
